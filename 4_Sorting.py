a14d493a052c905deb58fce66b6e5f255529fe67
Niloufar Alipour Tehrani <nalipour@cern.ch>
2017-11-27 17:57:36 +0100

    Sorting and using a function as key in the sorted function, using a nested function

--- 4_Sorting.py
+++ 4_Sorting.py
@@ -39,3 +39,21 @@ with open('/etc/passwd') as f:
             data.append((uname, int(uid)))
 
 sorted(data, key=mykey)
+
+# Use of nested functions
+
+
+def my_key(n):
+    def mykey(pair):
+        return pair[n]
+    return mykey
+
+
+data = []
+with open('/etc/passwd') as f:
+    for line in f:
+        if len(line.split(':')) > 3:
+            uname, _, uid, _ = line.split(':', 3)
+            data.append((uname, int(uid)))
+
+sorted(data, key=my_key(1))
