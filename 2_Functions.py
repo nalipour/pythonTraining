a8f1476b07c0e40e9553accb358853f23bcd97a7
Niloufar Alipour Tehrani <nalipour@cern.ch>
2017-11-27 14:49:38 +0100

    more argument passing tests

--- 2_Functions.py
+++ 2_Functions.py
@@ -47,11 +47,19 @@ def one(*args):
     return args
 
 
+def one_1(a, b, *args):
+    return args
+
+
 def two(b=1, c=2):
     return b, c
 
 
 def three(*args, **kwds):
+    return args, kwds
+
+
+def three_1(a, *args, **kwds):
     return args, kwds
 
 
@@ -63,8 +71,36 @@ one(1, 2, 'x')
 two(c=9)
 three("hello")
 
+type(three())
+type(three()[0])
+type(three()[1])
+three(1)  # returns a tuple and a keyword
+three(1, 3, b=3, banana="hello")
+
+
 one(*'hello')
 one(*range(3))
 two(**{'c': 'banana', 'b': 'apple'})
 three(*(list(range(3))+list(range(3))), **dict(a=1, b=2))
 one(*(range(10)))
+
+one_1(*"hello")
+one_1(*'hello', *'world')
+
+three_1(b=3, banana='hello', a=4)
+three_1(**{'b': 2, 'a': 999})
+len(three_1(b=3, banana='hello', a=4)[1])
+three_1(**dict(b=2, a=999))
+three_1(**dict(a=1, b=2), **dict(c=4, d=5))
+
+a, *rest = 'hello'
+print(a)
+print(rest)
+
+h, *rest, l, o = 'hello'
+print(h)
+print(rest)
+print(l)
+print(o)
+
+print(hash('hello'))
