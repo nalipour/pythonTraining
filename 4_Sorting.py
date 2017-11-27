5a2c7b806047345c9e265dfcfec2b041e2a1a8e9
Niloufar Alipour Tehrani <nalipour@cern.ch>
2017-11-27 17:01:22 +0100

    Sorting using listst

--- 4_Sorting.py
+++ 4_Sorting.py
@@ -9,3 +9,16 @@ with open('/etc/passwd') as f:
 
 sorted(d.keys())
 d
+
+# Use list for sorting
+
+data = list()
+
+with open('/etc/passwd') as f:
+    for line in f:
+        if len(line.split(':')) > 3:
+            uname, _, uid, _ = line.split(':', 3)
+            data.append((int(uid), uname))
+
+sorted(data)
+data
