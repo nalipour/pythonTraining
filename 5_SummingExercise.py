39a591c6eb5a441a7460724020d31132e0dcbcb4
Niloufar Alipour Tehrani <nalipour@cern.ch>
2017-11-28 09:38:19 +0100

    A general purpose function for addition, multiplication. IMPORTANT.

--- 5_SummingExercise.py
+++ 5_SummingExercise.py
@@ -1,6 +1,6 @@
 # Summing exercise
 from functools import reduce
-from operator import add
+from operator import add, mul
 
 
 def myint(token):
@@ -11,9 +11,32 @@ def myint(token):
 
 
 with open('numbers.txt') as f:
-    for line in f.readlines():
+    for line in f:
         tokens = line.split()
         numbers = tuple(map(myint, tokens))
 
         print(reduce(add, numbers))
         print(sum(numbers))
+
+# A general purpose code for multiplication, addition and strings
+# -------IMPORTANT-------
+
+
+def convert(identity_element):
+    def clever(token):
+        try:
+            return int(token)
+        except ValueError:
+            return identity_element
+    return clever
+
+
+# op, identityElement = add, 0
+op, identityElement = mul, 1
+
+with open('numbers.txt') as f:
+    for line in f:
+        tokens = line.split()
+        numbers = tuple(map(convert(identityElement), tokens))
+
+        print(reduce(op, numbers, identityElement))
