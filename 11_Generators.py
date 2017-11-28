842607ee61d5edf5b552900c95e475d424d15a1d
Niloufar Alipour Tehrani <nalipour@cern.ch>
2017-11-28 18:33:27 +0100

    Infinite iterative Fibonacci using generators

--- 11_Generators.py
+++ 11_Generators.py
@@ -14,3 +14,16 @@ b = gen_ints(10, 12)
 next(b)
 next(b)
 next(b)
+
+
+def fibg():
+    """ Infinite lazy Fibonacci: iterative Fib"""
+    yield 1
+    c, p = 1, 1
+    while True:
+        yield c
+        c, p = c+p, c
+
+
+f = fibg()
+[next(f) for x in range(10000)]
